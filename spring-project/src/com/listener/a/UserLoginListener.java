04785cadc8b79f8ac7eeded23de6f6a4795242c0
liutonghui <vip9renxing@hotmail.com>
2019-05-08 11:13:49 +0800

    listener

---
+++ spring-project/src/com/listener/a/UserLoginListener.java
@@ -0,0 +1,15 @@
+package com.listener.a;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @program: spring-project
+ * @description: 用户登录listener，记录在线用户数
+ * @author: lth
+ * @create: 2019-05-07 16:46
+ **/
+@Controller
+public class UserLoginListener  {
+
+
+}
--- spring-project/web/index.jsp
+++ spring-project/web/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>首页</title>
   </head>
   <body>
-  $END$
+        首页
   </body>
 </html>
